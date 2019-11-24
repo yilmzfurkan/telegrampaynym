92321f439ff3deda1ce2e9da48c0089dcea84858
yilmzfurkan <yilmzfurkan@gmail.com>
2019-11-25 01:15:39 +0300

    nothing important

--- paynym.py
+++ paynym.py
@@ -1,15 +1,24 @@
 #!/usr/bin/env python3
 from lxml import html
 import requests
+from requests import ConnectionError
 
-name = input("Name of the Paynym? ")
+try:
+	name = input("Name of the Paynym?: ")
+	page = requests.get('https://paynym.is/+' + name)
+	tree = html.fromstring(page.content)
+except ConnectionError:
+	print("The website doesn't respond. ")
+except KeyboardInterrupt:
+	print("Nothing has been typed. ")
 
-page = requests.get('https://paynym.is/+' + name)
-tree = html.fromstring(page.content)
+try:
+	paynymcode = tree.xpath('//span[@class="paycode"]/text()')
 
-paynymcode = tree.xpath('//span[@class="paycode"]/text()')
+	if len(paynymcode) == 1:
+		print(*paynymcode)
+	else:
+		print("No PayNym found. ")
 
-if len(paynymcode) == 1:
-	print (*paynymcode)
-else:
-	print("No PayNym Found")
+except NameError:
+	pass
